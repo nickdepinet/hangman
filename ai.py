9cf7b46123efabe98c9c03c95944b8e925e489b9
Nick Depinet <depinetnick@gmail.com>
2015-02-19 19:54:37 +0000

    AI working,ish

--- ai.py
+++ ai.py
@@ -1,3 +1,6 @@
+import sys
+import time
+
 from hangman import Hangman
 
 class AI():
@@ -5,7 +8,7 @@ class AI():
     words = {}
     freq = []
     possible = []
-    eng
+    eng = None
 
     def __init__(self, dictionary, man):
         self.eng = man
@@ -23,7 +26,11 @@ class AI():
         for p in self.possible:
             for x in range(len(self.eng.current)):
                 if not p[x] == self.eng.current[x] and not self.eng.current[x] == '_':
-                    self.possible.remove(p)    
+                    self.possible.remove(p)
+                    break
+        print "Possible:\n"
+        print self.possible
+        print "\n" 
 
     def move(self):
         freq_dict = {}
@@ -33,7 +40,7 @@ class AI():
                     freq_dict[c] += 1
                 else:
                     freq_dict[c] = 1
-        self.freq = [x for x in sorted(freq_dict, k=d.get)]
+        self.freq = [x for x in sorted(freq_dict, key=freq_dict.get)]
         next_ltr = self.freq.pop()
         while next_ltr in self.eng.failed or next_ltr in self.eng.current:
             next_ltr = self.freq.pop()
@@ -45,15 +52,21 @@ class AI():
         turns = 0
         while in_play:
             turns += 1
-            print self.eng
             self.move()
             in_play = not self.eng.won() and not self.eng.lost()
+            print self.eng
+            time.sleep(0.5)
         print self.eng
-        print "The computer won in " + turns + " turns.\n" if self.won() else "That word was too hard, the ai lost.\n"
+        print "The computer won in " + turns + " turns.\n" if self.eng.won() else "That word was too hard, the ai lost.\n"
 
 if __name__ == "__main__":
-    bot = AI('words.txt')
+    if not len(sys.argv) == 3:
+        print "Usage: python ai.py word_file goal_word"
+        sys.exit(1)
+    
+    man = Hangman(sys.argv[2].strip().lower())
+    bot = AI(sys.argv[1], man)
 
-    print bot.words[3]
+    bot.play()
 
 
