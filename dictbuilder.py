a3560f342ff56cebe9c3dfa02f85f3a6cb20ba53
Nick Depinet <depinetnick@gmail.com>
2015-03-10 13:48:50 +0000

    refactoring

--- dictbuilder.py
+++
@@ -1,8 +0,0 @@
-with open('google-books-common-words.txt') as f:
-    lines = set(tuple(line.strip().split()) for line in f)
-
-print len(lines)
-print lines.pop()
-print lines.pop()
-print lines.pop()
-print lines.pop()
